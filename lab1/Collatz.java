e5e2dac4a2390d0d8bf9417b169fd6d364c59d4d
kafka <don1379@outlook.com>
2023-09-20 14:44:40 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,12 +5,12 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
+        if (n % 2 == 0) {
+            return n / 2;
+        } else if (n % 2 == 1) {
             return 3 * n + 1;
         } else {
-            return n * 2;
+            return 1;
         }
     }
 
@@ -24,4 +24,5 @@ public class Collatz {
         System.out.println();
     }
 }
+
 
